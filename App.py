3b3c4dc5aadd114f31ed0e0c7141e79f1448b96f
Amonsalvem <alejandromonsalvemarin@gmail.com>
2025-02-27 07:20:34 -0500

    Update App.py

--- App.py
+++ App.py
@@ -5,4 +5,4 @@ st.title("myapp")
 
 st.header("En este espacio comienzo a desarrollar mis apllicaciones para interfaces multimodales.")
 st.write("Facilmente puedo realizar backend y frontend.")
-image = Image.open('interfaces Mult2.png')
+image = Image.open('Int.png')
