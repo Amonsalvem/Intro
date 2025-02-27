bb74ae68beb0581ac2fd63e7eba091911077d5bc
Amonsalvem <alejandromonsalvemarin@gmail.com>
2025-02-27 07:18:43 -0500

    Update App.py

--- App.py
+++ App.py
@@ -1,3 +1,8 @@
 import streamlit as st
+from PIL import Image
 
 st.title("myapp")
+
+st.header("En este espacio comienzo a desarrollar mis apllicaciones para interfaces multimodales.")
+st.write("Facilmente puedo realizar backend y frontend.")
+image = Image.open('interfaces Mult2.png')
